728519165727d402fd03d0828f3232df871a75b3
Erik Febianto <erik.febianto@nawadata.com>
2021-01-14 19:34:20 +0700

    AAA

---
+++ Object Repository/WIC/Page_/OK Kwn 1.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>OK Kwn 1</name>
+   <tag></tag>
+   <elementGuidId>151bb90b-d0ad-42f7-ae86-3338a65945cb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//table[@id='tableview-1299-record-103']/tbody/tr/td/div</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/WIC/Page_/iframe_Approval_nodeundefined_IFrame</value>
+   </webElementProperties>
+</WebElementEntity>
